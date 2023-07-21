316c85a4f0446ab9b28e2c99a7fcf77875425f85
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-21 21:28:56 +0545

    Re did the logic

--- logic.py
+++ logic.py
@@ -1,89 +1,52 @@
-import math,re
-π = math.pi
-e = math.e
+import re
+import math
 
 class Calculation:
-
-	def __init__(self, given):
-		self.given = given.replace(" ", "")
-		self.main()
-
-	def main(self):
-		self.given_list = [i for i in self.given ]
-
-		if "!" in self.given_list:
-			self.sub_factorial()
-
-		if "√" in self.given_list:
-			self.sub_uroot()
-
-		if "^" in self.given_list:
-			# Note here so i use this func for x^y in future too
-			self.power()
-
-
-		if "l" in self.given_list:
-			self.log()
-
-		# do the same for sin cos values too
-		c_list= ["s", "c", "t"]
-		if any((True for x in c_list if x in self.given_list )):
-			regex = "[a-zA-Z]+<\d+>"
-			match = re.finditer(regex, self.given)
-			for m in match:
-				functions = re.findall("[a-zA-Z]+", m.group())
-				values = re.findall("<\d+>", m.group())
-				for i,j in enumerate(functions):
-					val = f"{values[i].replace('<', '').replace('>', '')}"
-					val = math.radians(int(val))
-					to_replace=f'math.{j}({val})'
-					if j == "cot":
-						to_replace=f'math.tan(1/{val})' 
-					self.given = re.sub(regex,to_replace,self.given,count=1)
-
-		
-	def sub_factorial(self):
-		regex = "\d+!"
-		match = re.finditer(regex, self.given)
-		for m in match:
-			to_replace=f'math.factorial({m.group().replace("!", "")})'	
-			self.given = re.sub(regex,to_replace,self.given,count=1)
-
-	def sub_uroot(self):
-		regex = "√<\d+>"
-		match = re.finditer(regex, self.given)
-		for m in match:
-			digit = re.findall("\d+", m.group())[0]
-			to_replace = f'math.sqrt({digit})'
-			self.given = re.sub(regex,to_replace,self.given,count=1)
-
-	def power(self):
-		regex = "\d+\^<\d+>"
-		match = re.finditer(regex, self.given)
-		for m in match:
-			digits = re.findall("\d+", m.group())
-			to_replace = f'math.pow({digits[0]}, {digits[1]})'
-			self.given = re.sub(regex,to_replace,self.given,count=1)
-		check_e = re.finditer("e\^<\d+>", self.given)
-		if check_e:
-			for m in check_e:
-				digit = re.findall("\d+", m.group())[0]
-				to_replace = f'math.pow(e, {digit})'
-				self.given = re.sub("e\^<\d+>", to_replace,self.given, count=1)
-	def log(self):
-		regex = "log\[\d+]<\d+>"
-		match = re.finditer(regex, self.given)
-		for m in match:
-			numbers = re.findall("\d+", m.group())
-			x = numbers[0]
-			y = numbers[1]
-			to_replace = f'math.log({x},{y})'
-			self.given = re.sub(regex,to_replace,self.given,count=1)
-
-	def result(self):
-		return eval(self.given)
-
-# Calculation("5^<20>+10^<5>")
-# Calculation("e^<5>+log[10]<5>+e+π+1+5+(96+36)*sin<23>+5!+(50+9)+6*10^<6>-√<4>*(5+3)+tan<50>+cos<60>")
-# Calculation("√<4> + 5 ")
-
+    def __init__(self, given):
+        self.given = given.replace(" ", "")
+        
+    def evaluate(self):        
+        result = self._do_factorials()  
+        result = self._do_sqrt(result)
+        result = self._do_powers(result)    
+        result = self._do_trig(result)
+        result = self._do_log(result)
+        
+        try:
+            return eval(result)
+        except:
+            return "Error"
+        
+    def _do_factorials(self):
+        regex = "\d+!"   
+        subs = lambda m: str(math.factorial(int(m.group()[:-1]))) 
+        return re.sub(regex, subs, self.given)
+            
+    def _do_sqrt(self): 
+        regex = "√<\d+>"     
+        subs = lambda m: str(math.sqrt(int(m.group().replace("√<","").replace(">",""))))   
+        return re.sub(regex, subs, self.given)
+        
+    def _do_powers(self):
+        regex = "\d+\^<\d+>"      
+        subs = lambda m:str(math.pow(int(m.group().split('^<')[0]),
+                                     int(m.group().split('^<')[1].replace(">",""))))
+        return re.sub(regex, subs, self.given)
+            
+    def _do_trig(self):     
+        regex = "[a-zA-Z]+<\d+>"        
+        subs = lambda m: self._trig_func(m.group())         
+        return re.sub(regex, subs, self.given)
+       
+    def _trig_func(self, match):
+        func, arg = match.split("<")[0], int(match.split("<")[1].replace(">",""))
+        arg = math.radians(arg)
+        if func == "cot":  
+            return str(math.tan(1/arg))
+        return str(getattr(math, func)(arg))  
+ 
+    def _do_log(self):
+       regex = "log\[\d+]<\d+>"       
+       subs = lambda m: str(math.log(int(m.group().split('[')[1].split(']')[0]),
+                                        int(m.group().split('<')[1].replace(">",""))))  
+       return re.sub(regex, subs, self.given)
